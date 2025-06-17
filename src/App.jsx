383ac288ab45ce4981cad0b3d44d5b0a8add614d
Usi the Storm <50480382+UsiTheStorm@users.noreply.github.com>
2025-06-17 11:15:31 +0100

    Edit styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
 import { useState } from 'react';
 
 import './App.css';
-import qestions from './questions.json';
+import questions from './questions.json';
+
+function FlashCards() {
+  return (
+    <div className="flashcards">
+      {questions.map((q) => (
+        <div key={q.id}>
+          <p>{q.question}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
+      <h1>Flashcards</h1>
       <FlashCards />
     </div>
   );
